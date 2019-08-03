31a39a5a0a95c01966c235f06d20a909adbb805b
Marvin Colgin <bpadmin@builtbp.com>
2019-08-03 12:50:53 -0700

    FIX: tests for test_process_setstatus()

--- backend/app/tests/test_endpoints.py
+++ backend/app/tests/test_endpoints.py
@@ -5,7 +5,7 @@ def test_process_setstatus(client, sample_tweet):
     res = client.put(f"/api/v1/process/setstatus/{sample_tweet.id}", data={'statusid': '45'})
     print(f'res:[{res.status_code}]')
     assert res.status_code == 200  # TODO: This should be 204, it's being set to 204 in the response, but it's coming back 200  # noqa: E501
-    assert json.loads(res.data.decode()) == []
+    assert json.loads(res.data.decode()) == {'id': 1, 'party': 'GOP', 'person': 'Trump', 'status': '45', 'tone_analytic': 0.0, 'tone_anger': 0.0, 'tone_confident': 0.0, 'tone_fear': 0.0, 'tone_joy': 0.0, 'tone_sadness': 0.0, 'tone_tentative': 0.0, 'tweet': 'Bigly'}
 
 
 def test_sample_tweet_fixture(sample_tweet):
