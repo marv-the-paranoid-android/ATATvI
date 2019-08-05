03cbcdb234c5f587a726956f8d5c855b1e75df9e
Merry Cimakasky <mkcimakasky@gmail.com>
2019-08-05 16:14:07 -0700

    polar area chart rendering

--- frontend/atatvi-frontend/src/components/graph.js
+++ frontend/atatvi-frontend/src/components/graph.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react'; 
 //import ReactDom from 'react-dom';
-import {VictoryArea, VictoryPolarAxis,VictoryChart, VictoryTheme, VictoryLabel} from 'victory'; 
+import {VictoryArea, VictoryPolarAxis,VictoryChart, VictoryTheme} from 'victory'; 
 
 const testData = [
-  {tone: "anger", percentage: "20"},
-  {tone: "fear", percentage: "15"},
-  {tone: "joy", percentage: "20"},
-  {tone: "sadness", percentage: "20"},
-  {tone: "analytic", percentage: "15"},
-  {tone: "confident", percentage: "10"},
-  {tone: "tentative", percentage: "5"}
+  { x: 1, y: 2},
+  { x: 2, y: 3},
+  { x: 3, y: 5},
+  { x: 4, y: 4},
+  { x: 5, y: 6}, 
+  { x: 6, y: 8}, 
+  { x: 7, y: 1}
   
 ];
 
@@ -17,29 +17,27 @@ class Graph extends Component {
   render(){
     return(
       <VictoryChart polar
+      domain={{x: [0, 7]}}
       theme={VictoryTheme.material}
-      //domain={{x: [0, 1]}}
-      height={400} width={400}
-      >
-        <VictoryPolarAxis dependentAxis
-          style={{
-          axis: {stroke: "none"},
-          //tickLabels: { fill: ""},
-           grid: { stroke: "grey", strokeDasharray: "4, 8" }}}
-        />
-
-        <VictoryPolarAxis
-        tickValues = {["Anger", "Fear", "Joy", "Sadness", "Analytic", "Confident", "Tentative"]}
-        />
-
-        <VictoryArea
       
+      //height={400} width={400}
+      >
+      <VictoryPolarAxis dependentAxis
+        style={{
+        axis: {stroke: "none"},
+        grid: { stroke: "grey", strokeDasharray: "4, 8" }}}
+      />
+
+      <VictoryPolarAxis
+        tickValues = {["Anger", "Fear", "Joy", "Sadness", "Analytic","Confident", "Tentative"]}
+      />
+
+      <VictoryArea
         data={testData}
-        style = {{
-          data: {fill: "#c43a31" }
-        }}
+        style={{data: {width: 1}}}
+        
   
-        />
+      />
       </VictoryChart>  
     ); 
   }
