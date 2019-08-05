009987e329f202f8a2363726b3afee53180806ee
Merry Cimakasky <mkcimakasky@gmail.com>
2019-08-05 14:09:40 -0700

    area polar chart rendering with dummy data

--- frontend/atatvi-frontend/src/components/graph.js
+++ frontend/atatvi-frontend/src/components/graph.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'; 
 //import ReactDom from 'react-dom';
-import {VictoryArea, VictoryChart, VictoryAxis, VictoryTheme} from 'victory'; 
+import {VictoryArea, VictoryPolarAxis,VictoryChart, VictoryTheme, VictoryLabel} from 'victory'; 
 
 const data2012 = [
   {quarter: 1, earnings: 13000},
@@ -12,20 +12,27 @@ const data2012 = [
 class Graph extends Component {
   render(){
     return(
-      <VictoryChart
+      <VictoryChart polar
       theme={VictoryTheme.material}
-      domainPadding={20}
+      
       >
-        <VictoryAxis
-        tickValues={[1,2,3,4]}
-        tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        <VictoryPolarAxis dependentAxis
+        //style={{axis: {stroke: "black"}}}
+        //tickValues={[1,2,3,4]}
+        //tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
         />
-        <VictoryAxis
-        dependentAxis
-        tickFormat={(x)=>(`$${x/1000}k`)}
+        <VictoryPolarAxis
+        // dependentAxis
+        // tickFormat={(x)=>(`$${x/1000}k`)}
         />
         <VictoryArea
+        //categories={{x: ["anger", "fear", "joy", "sadness", "analytic", "confident", "tentative"] }}
         data={data2012}
+        style = {{
+          data: {fill: "#c43a31" }
+        }}
+        //labels={["anger", "fear", "joy", "sadness", "analytic", "confident", "tentative"]}
+        //labelComponent={<VictoryLabel renderInPortal />}
         x="quarter" 
         y="earnings"
         />
