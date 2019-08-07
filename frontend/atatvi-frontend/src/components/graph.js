437e19f530b4d06f8f34d366e30cbed1958c6999
Marvin Colgin <bpadmin@builtbp.com>
2019-08-07 14:27:08 -0700

    WIP

--- frontend/atatvi-frontend/src/components/graph.js
+++ frontend/atatvi-frontend/src/components/graph.js
@@ -1,39 +1,38 @@
-import React, {Component} from 'react'; 
+import React, {Component} from 'react';
 //import ReactDom from 'react-dom';
-import {VictoryArea, VictoryPolarAxis,VictoryChart, VictoryTheme} from 'victory'; 
+import {VictoryArea, VictoryPolarAxis,VictoryChart, VictoryTheme} from 'victory';
 
 // const testData = [
 //   { x: 1, y: .4},
 //   { x: 2, y: .9},
 //   { x: 3, y: .2},
 //   { x: 4, y: .6},
-//   { x: 5, y: .1}, 
-//   { x: 6, y: .8}, 
+//   { x: 5, y: .1},
+//   { x: 6, y: .8},
 //   { x: 7, y: .1}
-  
+
 // ];
 
 class Graph extends Component {
 
   constructor(props){
-    super(props); 
+    super(props);
     this.state = {
-      data: props.data,
-      color: props.color
+        party: props.party
     }
-    console.log("in graph, just props "+JSON.stringify(props))
-    console.log("in graph, props.data "+JSON.stringify(props.data))
+    console.log(this.state)
   }
 
   render(){
     return(
-      <VictoryChart polar
-      domain={{x: [0, 7]}}
-      theme={VictoryTheme.material}
-      
-      //height={400} width={400}
+      <VictoryChart
+        polar
+        domain={{x: [0, 7]}}
+        theme={VictoryTheme.material}
+        //height={400} width={400}
       >
-      <VictoryPolarAxis dependentAxis
+      <VictoryPolarAxis
+        dependentAxis
         style={{
         axis: {stroke: "none"},
         grid: { stroke: "grey", strokeDasharray: "4, 8" }
@@ -41,16 +40,16 @@ class Graph extends Component {
       />
 
       <VictoryPolarAxis
-        tickValues = {["Anger", "Fear", "Joy", "Sadness", "Analytic","Confident", "Tentative"]}
+        tickValues = {["Anger", "Fear", "Joy", "Sadness", "Analytic", "Confident", "Tentative"]}
       />
 
       <VictoryArea
         data={this.state.data}
         style={{data: {fill: this.state.color, width: 1}}}
       />
-      </VictoryChart>  
-    ); 
+      </VictoryChart>
+    );
   }
 }
 
-export default Graph; 
+export default Graph;
