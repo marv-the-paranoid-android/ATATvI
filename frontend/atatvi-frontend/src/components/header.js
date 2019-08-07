1c4ae0d8e64dabf19e008418a7f3502249672631
Raven Robertson <ravewillow6383@gmail.com>
2019-08-07 11:22:09 -0700

    added header component

---
+++ frontend/atatvi-frontend/src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Fragment, Component } from 'react'
+
+
+class Header extends Component {
+    render() {
+      return (
+      <Fragment>
+        <h1 className='title'>ATATvI</h1>
+        <p className='enter'>ATATvI a Natural Language Processing application</p>
+      </Fragment>
+      );
+    }
+  }
+
+export default Header
