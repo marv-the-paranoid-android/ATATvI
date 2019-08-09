4a7ab70084ee30e09e542e4c96417000b7db2abc
Raven Robertson <ravewillow6383@gmail.com>
2019-08-09 00:27:07 -0700

    added main component for paragraph content

---
+++ frontend/atatvi-frontend/src/components/Main.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+      return (
+        <>
+        <div className="about-us">
+        <main className="main">
+            <p>ATATvI</p>
+            </main>
+
+            </div>
+        </>
+      );
+    }
+  }
+
+  export default Main;
